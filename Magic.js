d59e7ff550e437b79e5da30c6e5adb4a026fb713
akhilbhardwaj23 <akhilbhardwaj23@gmal.com>
2015-01-25 22:40:57 +0530

    playing with bootstrap

---
+++ Magic.js
@@ -0,0 +1,23 @@
+﻿
+
+function parseRSS(url) {
+    $.ajax({
+        url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+        dataType: 'json',
+        success: function (data) {
+            callback(data.responseData.feed);
+        }
+    });
+}
+function callback(data) {
+
+
+}
+
+$(document).ready(
+  function () {
+      var urls = [];
+
+      urls.push("http://blog.hackerearth.com/feed");
+      urls.forEach(parseRSS, this);
+  });
